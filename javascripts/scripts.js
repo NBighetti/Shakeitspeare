b2e6f1e97fa31a78164fd0538a7ec19197fa45ef
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-05-30 11:35:11 -0500

    changes fonts

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,21 +1,26 @@
-//load poem
+
 $(document).ready(function(){
+
+// INSTRUCTS USER TO CLICK TO SHOW POEM
   $('#welcome').on('click', function(){
     $('#welcome').hide();
     getPoem();
     $('#poem').show();
   });
 
+// DISPLAYS SPEACH BUBBLE AND POEM
   $('#poem').on('click', function(){
     getPoem();
   })
 
+// REMOVES CLASS FROM SHAKESPEARE TO MAINTAIN DOC. STRUCTURE
   window.setTimeout(function(){
     $('img').removeClass("animated slideInUp");
   }, 1000)
 });
 
 
+//=====================================AJAX CALL TO GET POEM
 function getPoem(){
   $.ajax({
     url:'http://ShakeItSpeare.com/api/poem',
